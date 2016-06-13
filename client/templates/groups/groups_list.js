37739fe7777230b68543ba2aa210f10c62e5f138
linhan <yonglin12@hotmail.com>
2016-06-13 12:34:00 +0800

    groups template

---
+++ client/templates/groups/groups_list.js
@@ -0,0 +1,18 @@
+var groupsData = [
+	{
+		title: "Group ABC"
+	},
+	{
+		title: "0xDEADBEEF"
+	},
+	{
+		title: "tooeasyforedmund"
+	},
+	{
+		title: "Group XYZ"
+	}
+];
+
+Template.groupsList.helpers({
+	groups: groupsData
+});
