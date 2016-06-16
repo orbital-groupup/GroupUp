333b8815eb2888e354a79cb6409d50b39b90bb9c
linhan <yonglin12@hotmail.com>
2016-06-16 22:38:24 +0800

    validation for group title edit

--- client/templates/groups/group_edit.js
+++ client/templates/groups/group_edit.js
@@ -1,3 +1,16 @@
+Template.groupEdit.created = function() {
+	Session.set('groupEditErrors', {});
+}
+
+Template.groupEdit.helpers({
+	errorMessage: function(field) {
+		return Session.get('groupEditErrors')[field];
+	},
+	errorClass: function(field) {
+		return !!Session.get('groupEditErrors')[field] ? 'has-error': '';
+	}
+});
+
 Template.groupEdit.events({
 	'submit form': function(e) {
 		e.preventDefault();
@@ -7,6 +20,10 @@ Template.groupEdit.events({
 		var groupProperties = {
 			title: $(e.target).find('[name=title]').val()
 		};
+
+		var errors = validateGroup(groupProperties);
+		if (errors.title)
+			return Session.set('groupEditErrors', errors);
 
 		Groups.update(currentGroupId, {$set: groupProperties}, function(error) {
 			if (error)
