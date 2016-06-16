9d0b81437bd3398f8834007679f0e6b8430e036a
linhan <yonglin12@hotmail.com>
2016-06-16 22:14:44 +0800

    group title validation

--- client/templates/groups/group_submit.js
+++ client/templates/groups/group_submit.js
@@ -1,3 +1,16 @@
+Template.groupSubmit.created = function() {
+	Session.set('groupSubmitErrors', {});
+}
+
+Template.groupSubmit.helpers({
+	errorMessage: function(field) {
+		return Session.get('groupSubmitErrors')[field];
+	},
+	errorClass: function(field) {
+		return !!Session.get('groupSubmitErrors')[field]? 'has-error': '';
+	}
+});
+
 Template.groupSubmit.events({
 	'submit form': function(e) {
 		e.preventDefault();
@@ -6,6 +19,10 @@ Template.groupSubmit.events({
 			title: $(e.target).find('[name=title]').val()
 		};
 
+		var errors = validateGroup(group);
+		if (errors.title)
+			return Session.set('groupSubmitErrors', errors);
+		
 		Meteor.call('groupInsert', group, function(error, result) {
 			if (error) {
 				return throwError(error.reason);
