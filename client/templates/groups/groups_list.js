5ad5e730088c0766f873b4815fe1dca3457141ba
linhan <yonglin12@hotmail.com>
2016-07-03 22:40:19 +0800

    modal for create group

--- client/templates/groups/groups_list.js
+++ client/templates/groups/groups_list.js
@@ -1,5 +1,35 @@
+Template.groupsList.created = function() {
+	Session.set('groupSubmitErrors', {});
+}
+
 Template.groupsList.helpers({
+	errorMessage: function(field) {
+		return Session.get('groupSubmitErrors')[field];
+	},
+	errorClass: function(field) {
+		return !!Session.get('groupSubmitErrors')[field]? 'has-error': '';
+	},
 	groups: function() {
 		return Groups.find({ "members.name": Meteor.user().username});
+	}
+});
+
+Template.groupsList.events({
+	'click button#groupSubmitBtn': function(e) {
+		e.preventDefault();
+
+		var group = {
+			title: $("#groupSubmitModal").find('[name=title]').val()
+		};
+
+		var errors = validateGroup(group);
+		
+		Meteor.call('groupInsert', group, function(error, result) {
+			if (error) {
+				return throwError(error.reason);
+			}
+			$( '#groupSubmitModal' ).modal( 'hide' );
+  			$( '.modal-backdrop' ).fadeOut();
+		})
 	}
 });
