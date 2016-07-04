88b6601f9515f165fa3812dedbd554ab7050ddf9
Edmund <edmundmok@outlook.com>
2016-07-04 16:35:56 +0800

    Removed a link from upload

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -11,8 +11,10 @@ Template.notificationItem.helpers({
 	notificationPath: function(){
 		if (this.type==="upload")
 			return Router.routes.fileUpload.path({_id: this.groupId});
-		else
+		else if (this.type==="task")
 			return Router.routes.groupPage.path({_id: this.groupId});
+		else
+			return Router.routes.events.path({_id: this.groupId});
 	},
 
 	isEvent: function(){
